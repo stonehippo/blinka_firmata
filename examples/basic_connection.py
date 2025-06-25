fa3bb7697ba4a355db0eec6eb8b2da628542e943
George White <stonehippo@gmail.com>
2025-06-25 01:57:58 -0400

    Update basic example

--- examples/basic_connection.py
+++ examples/basic_connection.py
@@ -5,6 +5,10 @@ from blinka_firmata.firmata import Firmata
 # This assumes the standard baud rate for StandardFirmata of 115200bps
 board = Firmata()
 
+# If you're running something like StandardFirmata, it'll be slower
+# but we can still find it
+# board = Firmata(baud=57600)
+
 # Optionally select a specific device or at a given data rata
 # board = Firmata(port="/dev/ttyUSB0", baud=57600)
 
