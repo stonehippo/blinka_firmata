a7dedb85bbe4fb77d126dbdc1572272549cf48dc
George White <stonehippo@gmail.com>
2025-12-19 22:53:24 -0500

    Fix comments regarding baud rate and data rate

--- examples/basic_connection.py
+++ examples/basic_connection.py
@@ -2,14 +2,14 @@ import asyncio
 from blinka_firmata.firmata import Firmata
 
 # Find and connect to the first serial port with a Firmata-compatible device
-# This assumes the standard baud rate for StandardFirmata of 115200bps
+# This assumes the standard baud rate for ConfigurableFirmata of 115200bps
 board = Firmata()
 
 # If you're running something like StandardFirmata, it'll be slower
 # but we can still find it
 # board = Firmata(baud=57600)
 
-# Optionally select a specific device or at a given data rata
+# Optionally select a specific device or at a given data rate
 # board = Firmata(port="/dev/ttyUSB0", baud=57600)
 
 async def main():
